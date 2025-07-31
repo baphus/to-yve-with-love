eea10c67960d514aa9f460c7981d85c690604897
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2025-07-31 08:21:49 +0000

    more cute stickers!! animate the stickers like its a 2 frames just tilti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,14 @@ export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-background">
        {/* Sticker images that will float around */}
-      <Image src="/stickers/sticker1.png" alt="sticker" width={64} height={64} className="sticker top-1/3 left-24 -rotate-12" />
-      <Image src="/stickers/sticker2.png" alt="sticker" width={64} height={64} className="sticker top-2/3 right-32 rotate-6" />
-      <Image src="/stickers/sticker3.png" alt="sticker" width={64} height={64} className="sticker bottom-1/4 left-16 rotate-12" />
-      <Image src="/stickers/sticker4.png" alt="sticker" width={64} height={64} className="sticker bottom-1/2 right-12 -rotate-6" />
+      <Image src="/stickers/sticker1.png" alt="sticker" width={64} height={64} className="sticker top-[15%] left-[5%] -rotate-12" style={{ animationDelay: '0s' }} />
+      <Image src="/stickers/sticker2.png" alt="sticker" width={80} height={80} className="sticker top-[5%] right-[8%]" style={{ animationDelay: '-1s' }} />
+      <Image src="/stickers/sticker3.png" alt="sticker" width={56} height={56} className="sticker top-[30%] right-[15%] rotate-12" style={{ animationDelay: '-2s' }} />
+      <Image src="/stickers/sticker4.png" alt="sticker" width={72} height={72} className="sticker top-[60%] left-[10%]" style={{ animationDelay: '-3s' }} />
+      <Image src="/stickers/sticker5.png" alt="sticker" width={64} height={64} className="sticker top-[85%] right-[5%] -rotate-6" style={{ animationDelay: '-4s' }} />
+      <Image src="/stickers/sticker6.png" alt="sticker" width={88} height={88} className="sticker top-[70%] left-[25%] rotate-6" style={{ animationDelay: '-5s' }} />
+      <Image src="/stickers/sticker7.png" alt="sticker" width={60} height={60} className="sticker top-[90%] left-[12%]" style={{ animationDelay: '-6s' }} />
+
 
       <main className="flex-1">
         <HeroSection />
