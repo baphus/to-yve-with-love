db4c3a3cbcab105f64b9f50086b79376c4e4fe28
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2025-07-31 10:27:31 +0000

    no, i added sticker7-10 to public/stickers, make it viewable. also the h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
 
   useEffect(() => {
     setIsClient(true);
-    // Pre-create the audio element
     if (!audioRef.current) {
       audioRef.current = new Audio();
       audioRef.current.preload = 'auto';
@@ -26,7 +25,6 @@ export default function Home() {
   const handleUnlock = () => {
     setIsUnlocked(true);
     if (audioRef.current) {
-      // Set the source and play on unlock
       audioRef.current.src = '/oursong.mp3';
       audioRef.current.play().catch(error => {
         console.error("Audio autoplay failed:", error);
@@ -38,23 +36,7 @@ export default function Home() {
     <div className="flex min-h-screen w-full flex-col bg-background">
       {!isUnlocked && <EntryPuzzle onUnlock={handleUnlock} />}
       
-      {/* The audio element is now managed via the ref and not rendered directly here */}
-
       <div className={`transition-opacity duration-1000 ${isUnlocked ? 'opacity-100' : 'opacity-0'}`}>
-        {/* Sticker images that will float around */}
-        <Image src="/stickers/sticker1.png" alt="sticker" width={64} height={64} className="sticker top-[15%] left-[5%] -rotate-12 z-20" style={{ animationDelay: '0s' }} />
-        <Image src="/stickers/sticker2.png" alt="sticker" width={80} height={80} className="sticker top-[5%] right-[8%] z-20" style={{ animationDelay: '0.2s' }} />
-        <Image src="/stickers/sticker3.png" alt="sticker" width={56} height={56} className="sticker top-[30%] right-[15%] rotate-12 z-20" style={{ animationDelay: '0.4s' }} />
-        <Image src="/stickers/sticker4.png" alt="sticker" width={72} height={72} className="sticker top-[60%] left-[10%] z-20" style={{ animationDelay: '0.6s' }} />
-        <Image src="/stickers/sticker5.png" alt="sticker" width={64} height={64} className="sticker top-[85%] right-[5%] -rotate-6 z-20" style={{ animationDelay: '0.8s' }} />
-        <Image src="/stickers/sticker6.png" alt="sticker" width={88} height={88} className="sticker top-[70%] left-[25%] rotate-6 z-20" style={{ animationDelay: '1s' }} />
-        <Image src="/stickers/sticker7.png" alt="sticker" width={60} height={60} className="sticker top-[90%] left-[12%] z-20" style={{ animationDelay: '1.2s' }} />
-        <Image src="/stickers/sticker4.png" alt="sticker" width={48} height={48} className="sticker top-[45%] right-[5%] z-20" style={{ animationDelay: '1.4s' }} />
-        <Image src="/stickers/sticker1.png" alt="sticker" width={52} height={52} className="sticker top-[75%] right-[15%] z-20" style={{ animationDelay: '1.6s' }} />
-        <Image src="/stickers/sticker2.png" alt="sticker" width={58} height={58} className="sticker top-[25%] left-[20%] rotate-12 z-20" style={{ animationDelay: '1.8s' }} />
-        <Image src="/stickers/sticker7.png" alt="sticker" width={66} height={66} className="sticker top-[55%] right-[20%] -rotate-12 z-20" style={{ animationDelay: '2.0s' }} />
-        <Image src="/stickers/sticker3.png" alt="sticker" width={50} height={50} className="sticker top-[95%] left-[30%] rotate-6 z-20" style={{ animationDelay: '2.2s' }} />
-
         <main className="flex-1">
           <HeroSection />
           <LoveLettersSection />
